15ace785fa63a43d0699213b43c7448ba3d3cf50
Tyler Parret True <mysteryworldgod@outlook.com>
2024-04-09 21:52:07 +0800

    [Comments] Add comments
    添加了文件头部提示
    添加了一些注释

--- ExampleBasic/ExampleBasic.cpp
+++ ExampleBasic/ExampleBasic.cpp
@@ -1,4 +1,23 @@
-﻿
+﻿/*
+*    WGC-Capture-with-OpenCV
+*
+*     Copyright 2023-2024  Tyler Parret True
+*
+*    Licensed under the Apache License, Version 2.0 (the "License");
+*    you may not use this file except in compliance with the License.
+*    You may obtain a copy of the License at
+*
+*        http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS,
+*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*    See the License for the specific language governing permissions and
+*    limitations under the License.
+*
+* @Authors
+*    Tyler Parret True <mysteryworldgod@outlook.com><https://github.com/OwlHowlinMornSky>
+*/
 
 // You can use Spy++ to get the info about target window.
 constexpr wchar_t TargetWindowName[] = L"ExampleName";
@@ -25,11 +44,11 @@ int main() {
 	}
 	r_capture->setClipToClientArea(true);
 	// Run
-	r_capture->askForRefresh();
+	r_capture->askForRefresh(); // Ask for the first.
 	MSG msg{ 0 };
 	size_t testCnt = 3300; // About 100 seconds.
 	while (testCnt > 0) {
-		if (PeekMessageW(&msg, NULL, NULL, NULL, PM_REMOVE)) {
+		if (PeekMessageW(&msg, NULL, NULL, NULL, PM_REMOVE)) { // Neccessary for get frames.
 			TranslateMessage(&msg);
 			DispatchMessageW(&msg);
 		}
@@ -39,7 +58,7 @@ int main() {
 				if (r_capture->copyMatTo(mat, false)) {
 					cv::imshow("show", mat);
 				}
-				r_capture->askForRefresh();
+				r_capture->askForRefresh(); // Ask for next one.
 			}
 			Sleep(30);
 			--testCnt;
--- ExampleWin32/targetver.h
+++ ExampleWin32/targetver.h
@@ -1,4 +1,24 @@
-﻿#pragma once
+﻿/*
+*    WGC-Capture-with-OpenCV
+*
+*     Copyright 2023-2024  Tyler Parret True
+*
+*    Licensed under the Apache License, Version 2.0 (the "License");
+*    you may not use this file except in compliance with the License.
+*    You may obtain a copy of the License at
+*
+*        http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS,
+*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*    See the License for the specific language governing permissions and
+*    limitations under the License.
+*
+* @Authors
+*    Tyler Parret True <mysteryworldgod@outlook.com><https://github.com/OwlHowlinMornSky>
+*/
+#pragma once
 
 // // 包含 SDKDDKVer.h 可定义可用的最高版本的 Windows 平台。
 // 如果希望为之前的 Windows 平台构建应用程序，在包含 SDKDDKVer.h 之前请先包含 WinSDKVer.h 并
