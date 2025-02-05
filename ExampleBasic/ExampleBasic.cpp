fb694ca159a3ac7b104147f7e427e8aa4978ea4f
Tyler Parret True <mysteryworldgod@outlook.com>
2025-02-05 16:54:59 +0800

    Update ExampleBasic.cpp

--- ExampleBasic/ExampleBasic.cpp
+++ ExampleBasic/ExampleBasic.cpp
@@ -20,21 +20,22 @@
 */
 
 // You can use Spy++ to get the info about target window.
-constexpr wchar_t TargetWindowName[] = L"ExampleWindowName";
-constexpr wchar_t TargetWindowClass[] = L"ExampleClassName";
-constexpr bool    TestCaptureMonitor = true;
-constexpr bool    TestFreeThreaded = true;
+constexpr wchar_t TargetWindowName[] = L"崩坏：星穹铁道";
+constexpr wchar_t TargetWindowName2[] = L"原神";
+constexpr wchar_t TargetWindowClass[] = L"UnityWndClass";
+constexpr bool    TestCaptureMonitor = false;
+constexpr bool    TestFreeThreaded = false;
 
+#include <ohms/WGC.h>
 #include <opencv2/opencv.hpp>
 #include <Windows.h>
-#include <WGC/WGC.h>
 
 int TestNormal();
 int TestCallback();
 
 int main() { // You can switch the function.
-	return TestNormal();
-	//return TestCallback();
+	//return TestNormal();
+	return TestCallback();
 }
 
 size_t cnt = 0;
@@ -59,41 +60,24 @@ void Test(const cv::Mat& mat) {
 	//cv::imwrite(std::string("test\\") + std::to_string(cnt++) + ".png", mat);
 }
 
-void Test2(const cv::Mat& mat) {
-	if constexpr (TestCaptureMonitor) {
-		cv::Mat tmp;
-		mat.copyTo(tmp);
-		auto sz = tmp.size();
-		cv::resize(
-			tmp, tmp,
-			sz / 2,
-			0.0, 0.0, cv::InterpolationFlags::INTER_LINEAR
-		);
-		cv::imshow("show2", tmp);
-		cv::waitKey(1);
-	}
-	else {
-		cv::imshow("show2", mat);
-		cv::waitKey(1);
-	}
-	//cv::imwrite(std::string("test\\") + std::to_string(cnt++) + ".png", mat);
-}
-
 int TestNormal() {
 	// Initialization.
-	auto factory = wgc::IFactory::createInstance(false);
-	auto capture1 = factory->createCapturer().lock();
-	auto capture2 = factory->createCapturer().lock();
+	ohms::wgc::ICapture::setup(false);
+	auto r_capture = ohms::wgc::ICapture::getInstance();
 	// Find Window.
+<<<<<<< Updated upstream
 	HWND hwnd = FindWindowW(TargetWindowClass, TargetWindowName);
-	HWND hwnd2 = FindWindowW(TargetWindowClass, TargetWindowName2);
+=======
+	HWND hwnd = FindWindowW(0, L"MAA - v5.13.0-beta.4 - 相见欢#0128");
+	HWND hwnd2 = FindWindowW(0, L"Works.Debug [D:\\#COPY] – FreeFileSync 14.0");
+>>>>>>> Stashed changes
 
-	if constexpr (TestCaptureMonitor) {
+	if (TestCaptureMonitor) {
 		HMONITOR hmonitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTOPRIMARY);
 		if (hmonitor == NULL) {
 			return 2;
 		}
-		if (!capture1->startCaptureMonitor(hmonitor, TestFreeThreaded)) {
+		if (!r_capture->startCaptureMonitor(hmonitor, TestFreeThreaded)) {
 			return 3;
 		}
 	}
@@ -104,58 +88,63 @@ int TestNormal() {
 		if (!IsWindow(hwnd) || IsIconic(hwnd)) { // Requirements for capture window.
 			return 4;
 		}
-		capture1->setClipToClientArea(true);
-		capture2->setClipToClientArea(true);
-		if (!capture1->startCaptureWindow(hwnd, TestFreeThreaded)) {
+		r_capture->setClipToClientArea(true);
+		if (!r_capture->startCaptureWindow(hwnd, TestFreeThreaded)) {
 			return 5;
 		}
+<<<<<<< Updated upstream
+=======
 		if (!capture2->startCaptureWindow(hwnd2, TestFreeThreaded)) {
 			return 6;
-	}
+		}
+>>>>>>> Stashed changes
 	}
 
 	// Run
-	capture1->askForRefresh(); // Ask for the first.
-	capture2->askForRefresh(); // Ask for the first.
+	r_capture->askForRefresh(); // Ask for the first.
 	MSG msg{ 0 };
 	size_t testCnt = 3000; // About 10 seconds.
 	while (testCnt > 0) {
-		if (capture1->isRefreshed()) {
+		if (r_capture->isRefreshed()) {
 			cv::Mat mat;
-			capture1->copyMatTo(mat, true);
+<<<<<<< Updated upstream
+			if (r_capture->copyMatTo(mat, true)) {
 				Test(mat);
-			capture1->askForRefresh(); // Ask for next one.
 			}
+			r_capture->askForRefresh(); // Ask for next one.
+=======
+			capture1->copyMatTo(mat, true);
+			Test(mat);
+			capture1->askForRefresh(); // Ask for next one.
+		}
 		if (capture2->isRefreshed()) {
 			cv::Mat mat;
 			capture2->copyMatTo(mat, true);
 			Test2(mat);
 			capture2->askForRefresh(); // Ask for next one.
+>>>>>>> Stashed changes
 		}
-		cv::waitKey(20);
-		//--testCnt;
+		Sleep(20);
+		--testCnt;
 	}
 	// Clear.
-	//wgc::ICapture::drop();
+	ohms::wgc::ICapture::drop();
 	return 0;
 }
 
 int TestCallback() {
 	// Initialization.
-	auto factory = wgc::IFactory::createInstance(false);
-	auto capture1 = factory->createCapturer().lock();
-	auto capture2 = factory->createCapturer().lock();
+	ohms::wgc::ICapture::setup(false);
+	auto r_capture = ohms::wgc::ICapture::getInstance();
 	// Find Window.
 	HWND hwnd = FindWindowW(TargetWindowClass, TargetWindowName);
-	HWND hwnd2 = FindWindowW(TargetWindowClass, TargetWindowName2);
 
-	if constexpr (TestCaptureMonitor) {
+	if (TestCaptureMonitor) {
 		HMONITOR hmonitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTOPRIMARY);
 		if (hmonitor == NULL) {
 			return 2;
 		}
-
-		if (!capture1->startCaptureMonitorWithCallback(hmonitor, &Test)) {
+		if (!r_capture->startCaptureMonitorWithCallback(hmonitor, &Test)) {
 			return 3;
 		}
 	}
@@ -166,19 +155,15 @@ int TestCallback() {
 		if (!IsWindow(hwnd) || IsIconic(hwnd)) { // Requirements for capture window.
 			return 4;
 		}
-		capture1->setClipToClientArea(true);
-		capture2->setClipToClientArea(true);
-		if (!capture1->startCaptureWindowWithCallback(hwnd, &Test)) {
+		r_capture->setClipToClientArea(true);
+		if (!r_capture->startCaptureWindowWithCallback(hwnd, &Test)) {
 			return 5;
-		}
-		if (!capture2->startCaptureWindowWithCallback(hwnd2, &Test2)) {
-			return 6;
 		}
 	}
 
 	int a;
 	std::cin >> a;
 
-	//wgc::ICapture::drop();
+	ohms::wgc::ICapture::drop();
 	return 0;
 }
